396e7ce5c6a94b40094ba4cb328ee5b6c1b29575
Mohamed <42390271+MohamedDesign-1@users.noreply.github.com>
2025-01-21 15:37:09 +0200

    Fix navbar and make screens

---
+++ lib/config/di/di.config.dart
@@ -0,0 +1,33 @@
+// dart format width=80
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// **************************************************************************
+// InjectableConfigGenerator
+// **************************************************************************
+
+// ignore_for_file: type=lint
+// coverage:ignore-file
+
+// ignore_for_file: no_leading_underscores_for_library_prefixes
+import 'package:get_it/get_it.dart' as _i174;
+import 'package:injectable/injectable.dart' as _i526;
+
+import '../../core/api/api_manager.dart' as _i1057;
+import '../../features/mainLayout/manager/main_layout_cubit.dart' as _i574;
+
+extension GetItInjectableX on _i174.GetIt {
+// initializes the registration of main-scope dependencies inside of GetIt
+  _i174.GetIt init({
+    String? environment,
+    _i526.EnvironmentFilter? environmentFilter,
+  }) {
+    final gh = _i526.GetItHelper(
+      this,
+      environment,
+      environmentFilter,
+    );
+    gh.factory<_i574.MainLayoutCubit>(() => _i574.MainLayoutCubit());
+    gh.singleton<_i1057.ApiManager>(() => _i1057.ApiManager());
+    return this;
+  }
+}
