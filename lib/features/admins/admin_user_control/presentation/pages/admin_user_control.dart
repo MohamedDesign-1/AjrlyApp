c10521ce53abd2da028d174fb0a69cc2b136df26
Mohamed Mahmoud <m7medyt2017@gmail.com>
2025-02-19 09:55:36 +0200

    Finish admin_user_control page

--- lib/features/admins/admin_user_control/presentation/pages/admin_user_control.dart
+++ lib/features/admins/admin_user_control/presentation/pages/admin_user_control.dart
@@ -36,36 +36,128 @@ class AdminUserControl extends StatelessWidget {
             ),
           ),
         ),
-        body: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  CounterCard(
-                    sectionIcon: SvgAssets.deActiveUsers,
-                    sectionName: "عدد الحسابات المفعلة",
-                    sectionNum: "110",
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.all(8.0.w),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    CounterCard(
+                      sectionIcon: SvgAssets.downloadPdf,
+                      sectionName: "تحميل كملف PDF",
+                      sectionNum: "110",
+                    ),
+                    CounterCard(
+                      sectionIcon: SvgAssets.deActiveUsers,
+                      sectionName: "عدد الحسابات المعطلة",
+                      sectionNum: "110",
+                    ),
+                    CounterCard(
+                      sectionIcon: SvgAssets.activeUsers,
+                      sectionName: "عدد الحسابات المفعلة",
+                      sectionNum: "110",
+                    ),
+                    CounterCard(
+                      sectionIcon: SvgAssets.totalUsers,
+                      sectionName: "اجمالي المستخدمين",
+                      sectionNum: "110",
+                    ),
+                  ],
+                ),
+                SizedBox(height: 8.h),
+                Text("معلومات الحسابات",
+                    style: getBoldStyle(color: ColorManager.black, fontSize: 20.sp)),
+                SizedBox(height: 8.h),
+                Container(
+                  width: double.infinity,
+                  padding: EdgeInsets.all(8.0.w),
+                  decoration: BoxDecoration(
+                    color: ColorManager.lightgray,
+                    borderRadius: BorderRadius.all(Radius.circular(16.r)),
                   ),
-                  CounterCard(
-                    sectionIcon: SvgAssets.deActiveUsers,
-                    sectionName: "عدد الحسابات المفعلة",
-                    sectionNum: "110",
+                  child: Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Expanded(
+                            child: Text(
+                              "حالة الحساب",
+                              style: getBoldStyle(color: ColorManager.primary, fontSize: 16.sp),
+                              textAlign: TextAlign.center,
+                            ),
+                          ),
+                          Expanded(
+                            child: Text(
+                              "رقم الموبايل",
+                              style: getBoldStyle(color: ColorManager.primary, fontSize: 16.sp),
+                              textAlign: TextAlign.center,
+                            ),
+                          ),
+                          Expanded(
+                            child: Text(
+                              "الاسم بالكامل",
+                              style: getBoldStyle(color: ColorManager.primary, fontSize: 16.sp),
+                              textAlign: TextAlign.center,
+                            ),
+                          ),
+                          Expanded(
+                            child: Text(
+                              "رقم المستخدم",
+                              style: getBoldStyle(color: ColorManager.primary, fontSize: 16.sp),
+                              textAlign: TextAlign.center,
+                            ),
+                          ),
+                        ],
+                      ),
+                      Divider(thickness: 2),
+                      ListView.separated(
+                        shrinkWrap: true,
+                        physics: NeverScrollableScrollPhysics(),
+                        itemBuilder: (context, index) => Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Expanded(
+                              child: Text(
+                                "تعطيل",
+                                style: getBoldStyle(color: ColorManager.black, fontSize: 12.sp),
+                                textAlign: TextAlign.center,
+                              ),
+                            ),
+                            Expanded(
+                              child: Text(
+                                "0123456789",
+                                style: getBoldStyle(color: ColorManager.black, fontSize: 12.sp),
+                                textAlign: TextAlign.center,
+                              ),
+                            ),
+                            Expanded(
+                              child: Text(
+                                "محمد أحمد",
+                                style: getBoldStyle(color: ColorManager.black, fontSize: 12.sp),
+                                textAlign: TextAlign.center,
+                              ),
+                            ),
+                            Expanded(
+                              child: Text(
+                                "${index + 1}",
+                                style: getBoldStyle(color: ColorManager.black, fontSize: 12.sp),
+                                textAlign: TextAlign.center,
+                              ),
+                            ),
+                          ],
+                        ),
+                        separatorBuilder: (context, index) => Divider(thickness: 1),
+                        itemCount: 10,
+                      ),
+                    ],
                   ),
-                  CounterCard(
-                    sectionIcon: SvgAssets.deActiveUsers,
-                    sectionName: "عدد الحسابات المفعلة",
-                    sectionNum: "110",
-                  ),
-                  CounterCard(
-                    sectionIcon: SvgAssets.deActiveUsers,
-                    sectionName: "عدد الحسابات المفعلة",
-                    sectionNum: "110",
-                  ),
-                ],
-              )
-            ],
+                ),
+              ],
+            ),
           ),
         ));
   }
